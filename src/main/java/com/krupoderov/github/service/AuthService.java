6a6156e4c2fb8f3d9a67bf01fa837db37fb53572
KrupoderovMikhail <mishka.crash@gmail.com>
2019-11-10 17:33:08 +0500

    Added Password Encoder.

--- src/main/java/com/krupoderov/github/service/AuthService.java
+++ src/main/java/com/krupoderov/github/service/AuthService.java
@@ -3,6 +3,7 @@ package com.krupoderov.github.service;
 import com.krupoderov.github.dto.RegisterRequest;
 import com.krupoderov.github.model.User;
 import com.krupoderov.github.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,16 +13,22 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
         user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
